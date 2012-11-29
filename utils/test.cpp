2a89a3d3d219b16e2507235cc9e1621cbaa0302f
Karchnu <kane.root@gmail.com>
2012-11-29 12:54:27 +0100

    Bientôt les films en lecture ET en écriture, actuellement fonctionne en lecture

--- utils/test.cpp
+++ utils/test.cpp
@@ -1,5 +1,5 @@
-
 #include "fileutils.h"
+#include "film.h"
 
 using namespace std;
 
@@ -18,7 +18,32 @@ void file_utils()
 	f.write();
 }
 
+void film_test()
+{
+	Film film(2);
+	cout << "Film name : " << film.getName() << endl;
+	if(film.is3d())
+		cout << "Film en 3D" << endl;
+	else
+		cout << "Film non en 3D" << endl;
+
+	cout << "Film synopsis : " << film.getSynopsis() << endl;
+	cout << "Horaires : ";
+	vector<int> horaires(film.getHoraires());
+	for(int i(0) ; i < horaires.size() ; i++)
+		cout << horaires.at(i) << " ";
+	cout << endl;
+
+	vector<string> actors(film.getActors());
+	for(int i(0) ; i < actors.size() ; i++)
+		cout << actors.at(i) << " ";
+	cout << endl;
+
+	cout << " En une seule ligne : " << film.getActorsString() << endl;
+}
+
 int main(int argc, char **argv)
 {
-	file_utils();
+	//file_utils();
+	film_test();
 }
