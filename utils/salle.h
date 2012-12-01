f383c1383f04ddc21ef499347dba1dc75d22bfac
Karchnu <kane.root@gmail.com>
2012-12-02 00:25:01 +0100

    classe salle disponible

--- utils/salle.h
+++ utils/salle.h
@@ -1,14 +1,23 @@
 #ifndef __SALLE_H__
 #define __SALLE_H__
 
+#define SALLES_FILE_NAME "../data/salles"
+
 #include <vector>
 #include <iostream>
 
-class Salle : public
+#include "fileutils.h"
+
+class Salle
 {
 	public :
-		Salle(int id);
-		virtual ~Salle();
+		static void init();
+		static int getNbPlaces(int id);
+		static int getNbPlacesPrises(int id);
+		static void reserverPlace(int id);
+
+	private :
+		static FileUtils * salles;
 };
 
 #endif
