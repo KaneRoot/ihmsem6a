63ac617fe3b462756f96a9c020e65ff6c3f2fa98
KilianHett <kilian@octocore.(none)>
2012-12-11 23:20:34 +0100

    check horaire Prémplie

--- ui/detailEditWidget.cpp
+++ ui/detailEditWidget.cpp
@@ -56,15 +56,35 @@ void DetailEditWidget::editFilm()
 
 void DetailEditWidget::loadHoraire()
 {
+	vector<int> horaireFilm;
+	
+	if (this->mode == MODE_EDIT)
+	{
+		horaireFilm = film->getHoraires();
+	}	
+
 	vector<int> horaires = Horaire::getHorairesId();  
+	bool b;
 	for (unsigned int i=0;i<horaires.size();i++)
 	{
 		checks.push_back(new QCheckBox(
 					QString::fromUtf8(Horaire::getHoraire(horaires.at(i)).c_str()),
 					this->areaContents));
+		if (this->mode == MODE_EDIT)
+		{
+			b=false;
+			for (unsigned int j=0;j<horaireFilm.size() && !b;j++)
+			{
+				b = horaireFilm.at(j)==(int)i;
+			}
+			if (b)
+			{
+				checks.at(i)->setCheckState(Qt::Checked);
+			}
+		}
 		this->verticalLayout->addWidget(checks.at(checks.size()-1));
 	}
-	scrollArea->setWidget(areaContents);
+//	scrollArea->setWidget(areaContents);
 }
 
 void DetailEditWidget::selectAll()
